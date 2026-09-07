bf0d9ba669bbe30e99bf4bbe26d5427544f8f7e3
Ольга <nikulshina20.o@mail.ru>
2026-09-07 21:19:38 +0300

    Add character input

--- Nikulshina_AS_04/Nikulshina_AS_04/Nikulshina_AS_04.cpp
+++ Nikulshina_AS_04/Nikulshina_AS_04/Nikulshina_AS_04.cpp
@@ -6,6 +6,10 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    int x;
+    std::cin >> x;
+    if (x != 0);
+    std::cout << 10 / x;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
